c9a7592596c6d2e634d161d3069dabba48fe70dd
ccosneau0 <charles.cosneau@hetic.net>
2021-07-19 22:58:26 +0200

    add student dashboard

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Booking from './pages/booking';
 import Connexion from './pages/connexion';
 import Inscription from './pages/inscription'
+import StudentDashboard from './pages/studentDashboard';
 import './style/global.scss'
 import {
   BrowserRouter as Router,
@@ -9,6 +10,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import StudentHeader from './composants/studentHeader';
 
 function App() {
   return (
@@ -23,6 +25,9 @@ function App() {
           </Route>
           <Route path="/booking">
             <Booking />
+          </Route>
+          <Route path="/studentDashboard">
+            <StudentDashboard />
           </Route>
         </Switch>
       </Router>
---
+++ src/composants/studentHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NotificationBell from '../assets/img/notification-bell.svg'
+
+const StudentHeader = () => {
+  return(
+    <section className="studentHeader">
+      <h3 className="studentHeader-title">Ready2Work</h3>
+      <div className="studentHeader-profil">
+        <img className="studentHeader-notification" src={ NotificationBell } alt="Pictogramme cloche notifications"/>
+        <img className="studentHeader-picture" alt="photo de profil"/>
+        <p>Jones Ferdinand</p>
+      </div>
+    </section>
+  )
+}
+export default StudentHeader;
